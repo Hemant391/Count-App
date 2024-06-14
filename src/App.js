9a6aa62060e1a1843683af749b01ad5e0c6c4f62
Hemant391 <148564538+Hemant391@users.noreply.github.com>
2024-06-14 15:40:53 +0530

    by class component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@
 import React, { useState } from 'react'
 
 
-function App() {
-  const [number,setNumber]=useState(0)
-  return (
-    <div className="App">
-      <h1>Counter App</h1>
-      <p>Count: {number}</p>
-      <button type="button" onClick={()=>setNumber(number+1)}>Increment</button>
-      <button type="button" onClick={()=>setNumber(number-1)}>Decrement</button>
-    </div>
-  );
-}
+// function App() {
+//   const [number,setNumber]=useState(0)
+//   return (
+//     <div className="App">
+//       <h1>Counter App</h1>
+//       <p>Count: {number}</p>
+//       <button type="button" onClick={()=>setNumber(number+1)}>Increment</button>
+//       <button type="button" onClick={()=>setNumber(number-1)}>Decrement</button>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+// import App from './App';
 
+
+
+class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={count :0}
+  }
+
+  increment=()=>{
+    this.setState((prev)=>{
+      return {count : prev.count+1}
+    })
+  }
+  decrement=()=>{
+    this.setState((prev)=>{
+      return {count : prev.count-1}
+    })
+  }
+  
+  render(){
+    return (
+      <div >
+      <h1>Counter App</h1>
+      <p>Count: {this.state.count}</p>
+      <button type="button" onClick={this.increment}>Increment</button>
+      <button type="button" onClick={this.decrement}>Decrement</button>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <Counter />
 );
-
